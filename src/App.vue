f8c19f16f06fe607582acf783c62afa0bec41bd4
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2020-06-03 14:30:51 +0200

    DP-919: Removed vue-template stuff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="queue-predictions">
+    <Queue />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Queue from "./components/Queue.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Queue,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import "~@/assets/sass/fontStyle.scss";
+#queue-predictions {
+  font-family: "Oslo Light", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  color: black;
+  background-color: white;
 }
 </style>
