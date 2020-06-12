2bfa1e5a544498f6ba150ad14c5305567a848e93
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2020-06-12 11:11:59 +0200

    DP-925: If queue is null

--- src/components/QueueTime.vue
+++ src/components/QueueTime.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="queue-time">
-    <h1>Køtid - gjenbruksstasjon</h1>
-    <h2>{{ roundedUpTime }} minutter</h2>
+    <div v-for="queueTimeArray in mockData" :key="queueTimeArray.station_id">
+      <h1>{{ queueTimeArray.station_name }}</h1>
+      <div v-if="showQueue">
+        <h2>Køtiden er {{ roundedUpTime }} minutter</h2>
+      </div>
+      <div v-else>
+        <h2>Vi har ingen estimert køtid for øyeblikket</h2>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,12 +20,28 @@ export default {
   data() {
     return {
       time: "10.73525436",
-      url: "venter på url",
+      mockData: [
+        {
+          station_id: 42,
+          station_name: "Haraldrud gjenbruksstasjon",
+          is_open: true,
+          queue: {
+            is_full: false,
+            expected_time: 0.5,
+            min_time: 0.35,
+            max_time: 0.65,
+            updated_at: "2020-06-11T13:23:33.642441",
+          },
+        },
+      ],
     };
   },
   computed: {
     roundedUpTime: function() {
       return Math.round(this.time);
+    },
+    showQueue() {
+      return this.mockData[0].queue !== null;
     },
   },
 
