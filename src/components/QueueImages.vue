7f3b394ba70425d475183bdff7e9ec201179bb22
cecilie roras <cecilie.roras@byr.oslo.kommune.no>
2020-06-22 15:31:54 +0200

    DP-961: Added new components

---
+++ src/components/QueueImages.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="queue-images">
+    <div v-for="imageObjects in images" :key="imageObjects.station_id">
+      <p>
+        {{ imageObjects.station_id }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Queue-Images",
+  data() {
+    return {};
+  },
+  props: {
+    images: Array,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "~@/assets/sass/fontStyle.scss";
+.queue-images {
+  font-family: "Oslo Light", Helvetica, Arial, sans-serif;
+}
+p {
+  text-align: center;
+  margin-top: 10px;
+}
+
+h2 {
+  text-align: center;
+  margin-top: 10px;
+}
+</style>
