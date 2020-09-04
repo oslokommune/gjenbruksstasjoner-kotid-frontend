3725cddb12503eeaabde34c44eb72e3223e68b1b
Simen Heggestøyl <simen.heggestoyl@byr.oslo.kommune.no>
2020-09-04 09:45:13 +0200

    Append "/" to API URLs
    Our API URLs redirect to their "/"-appended equivalents when missing it,
    so let's just append them with "/" to begin with.

--- src/conf.js
+++ src/conf.js
@@ -4,14 +4,14 @@ if (process.env.VUE_APP_STAGE === "production") {
   conf = {
     urls: {
       // PROD URL
-      queueData: "https://api.data.oslo.systems/gjenbruksstasjoner-kotid",
+      queueData: "https://api.data.oslo.systems/gjenbruksstasjoner-kotid/",
     },
   };
 } else {
   conf = {
     urls: {
       // DEV URL
-      queueData: "https://api.data-dev.oslo.systems/gjenbruksstasjoner-kotid",
+      queueData: "https://api.data-dev.oslo.systems/gjenbruksstasjoner-kotid/",
     },
   };
 }
